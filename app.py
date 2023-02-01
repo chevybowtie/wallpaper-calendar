cd8c03bf978dcf5bb08d71ebe202901e03685aaa
Paul Sturm <paul.sturm@cotton-software.com>
2023-01-31 22:53:39 -0600

    intial outlook logic

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import ctypes
-import datetime
+import datetime as dt
 import os
 import platform
 import random
 import calendar
 
+# Windows APIs
+import win32com.client
+
 import subprocess
 # pip install wget
-import wget       
+import wget
 
 # from the pillow library - pip install pillow
 from PIL import Image, ImageDraw, ImageFont
 
 import config
 
+
 def execute_set(command):
     subprocess.call(["/bin/bash", "-c", command])
+
 
 def set_wallpaper(wallpaper_name='\output.jpg'):
     cwd = os.getcwd()
@@ -23,33 +28,67 @@ def set_wallpaper(wallpaper_name='\output.jpg'):
         ctypes.windll.user32.SystemParametersInfoW(
             20, 0, cwd+wallpaper_name, 0)
     elif platform.system() == 'Linux':
-        execute_set("/usr/bin/gsettings set org.gnome.desktop.background picture-uri " + cwd + wallpaper_name )
+        execute_set(
+            "/usr/bin/gsettings set org.gnome.desktop.background picture-uri " + cwd + wallpaper_name)
     else:
         input('Your operating system is not supported')
 
 
 def create_wallpaper():
+
     # Load font
     fontFile = 'fonts/{}'.format(config.default_font)
     font = ImageFont.truetype(fontFile, config.font_size)
+    heroFont = ImageFont.truetype(fontFile, config.font_size * 4)
+    today = dt.date.today()
+
     # Load Image
     wallpaper_name = get_wallpaper()
     image = Image.open(wallpaper_name)
     draw = ImageDraw.Draw(image)
-    # Gregorian Datetime
-    if config.write_gregorian_datetime:
-        gtoday = str(datetime.datetime.now().date())
-        w, h = draw.textsize(text=gtoday, font=font)
-        # draw.text((image.width-w-config.position_for_gdatetime[1], image.height-h-config.position_for_gdatetime[1]),
-        #           gtoday, (255, 255, 255), font=font)
-        draw.text((image.width-w-config.position_for_gdatetime[0], image.height-h-config.position_for_gdatetime[1]),
+
+    # Show calendar for this and next month
+    if config.write_gregorian_this_month:
+        gtoday = str(today)
+        w, h, *z = draw.textbbox((0, 0), gtoday, font)
+        draw.text((image.width-w-config.position_for_calendar[0], image.height-h-config.position_for_calendar[1]),
                   calendar.month(2023, 1), (255, 255, 255), font=font)
+
         if config.write_gregorian_next_month:
-            gtoday = str(datetime.datetime.now().date())
-            draw.text((image.width-w-config.position_for_gdatetime[0], image.height-h-config.position_for_gdatetime[1] + 300),
-                      calendar.month(2023, 2), (255, 255, 255), font=font)
+            nextMonth = next_month_date(today)
+            draw.text((image.width-w-config.position_for_calendar[0], image.height-h-config.position_for_calendar[1] + 300),
+                      calendar.month(nextMonth.year, nextMonth.month), (255, 255, 255), font=font)
+
+    # if enabled, show today's appointments from Outlook
+    if config.write_todays_appts:
+        appts = get_outlook_appointments(
+            dt.datetime(2023, 1, 31), dt.datetime(2023, 2, 1))
+        draw.text((config.position_for_appts[0], config.position_for_appts[1]),
+                  appts, (255, 255, 255), font=font)
+
+    # if enabled, show today's data (large)
+    if config.write_today_big:
+        draw.text((config.position_for_today[0], config.position_for_today[1]+100), str(dt.datetime.today().day),
+                  (255, 255, 255), font=heroFont)
+        draw.text((config.position_for_today[0], config.position_for_today[1]), calendar.day_name[dt.datetime.today().weekday()],
+                  (255, 255, 255), font=heroFont)
+
+    # create the image
     draw = ImageDraw.Draw(image)
+
+    # output to disk
     image.save("output.jpg")
+
+
+def next_month_date(d):
+    year = d.year+(d.month//12)
+    month = 1 if (d.month//12) else d.month + 1
+    next_month_len = calendar.monthrange(year, month)[1]
+    next_month = d
+    if d.day > next_month_len:
+        next_month = next_month.replace(day=next_month_len)
+    next_month = next_month.replace(year=year, month=month)
+    return next_month
 
 
 def get_wallpaper():
@@ -66,6 +105,51 @@ def get_wallpaper():
             return 'wallpapers/' + config.default_wallpaper
     except:
         return 'wallpapers/default.jpg'
+
+
+def get_outlook_appointments(begin, end):
+    # get a handle on Outlook
+    outlook = win32com.client.Dispatch(
+        'Outlook.Application').GetNamespace('MAPI')
+
+    # 9 = list of all the meetings, 6 = emails
+    calendar = outlook.getDefaultFolder(9).Items
+
+    # events that repeat
+    calendar.IncludeRecurrences = True
+
+    # sort
+    calendar.Sort("[Start]")
+
+    # setup a constraint
+    calendar.Sort('[Start]')
+    restriction = "[Start] >= '" + begin.strftime(
+        '%m/%d/%Y') + "' AND [END] <= '" + end.strftime('%m/%d/%Y') + "'"
+    calendar = calendar.Restrict(restriction)
+
+    return calendar
+
+
+def groom_appointments(calendar):
+    appointmentDictionary = {}
+
+    for appointment in calendar:
+        meetingDate = str(appointment.Start)
+        subject = str(appointment.Subject)
+        duration = str(appointment.duration)
+        # date = parse(meetingDate).date()
+        # time = parse(meetingDate).time()
+        appointmentDictionary[subject] = {"Subject": [
+            subject], "Time": [meetingDate], "Durations": [duration]}
+
+    for subject in appointmentDictionary.keys():
+        rowDict = {}
+        rowDict["Subject"] = appointmentDictionary[subject]["Subject"] if appointmentDictionary[subject]["Subject"] else ""
+        ApptRow(rowDict)
+
+
+def ApptRow(rowDictionary):
+    print(rowDictionary)
 
 
 def start():
--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 # Wallpaper configs
-static_wallpaper = True
-online_random_wallpaper = True
-offline_random_wallpaper = False
+static_wallpaper = False
+online_random_wallpaper = False
+offline_random_wallpaper = True
 default_wallpaper = 'default.jpg'
 system_resolution = (1920,1080)
 
 ## Content
 
 # text
-write_gregorian_datetime = True
+write_gregorian_this_month = True
 write_gregorian_next_month = True
+write_today_big = True
 font_size = 30
 default_font = 'Kingthings Trypewriter 2.ttf'
 
-# Gregorian Datetime
-position_for_gdatetime = (275, 600) #Change this to see G-Date
+# Gregorian calendar position
+position_for_calendar = (400, 600)
+position_for_today = (50, 10)
+
+# Windows Outlook integration
+write_todays_appts = False
+position_for_appts = (50, 600)
